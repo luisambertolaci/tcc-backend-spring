f94b677d9cfeb0e1fa9e66869043e14d07d55312
luisambertolaci <luisabertolaci@gmail.com>
2019-02-28 17:53:38 -0300

    Update CriandoPerfis.java
    criando vetor com todas as tags votadas

--- src/main/java/tcc/ifes/algoritmos/CriandoPerfis.java
+++ src/main/java/tcc/ifes/algoritmos/CriandoPerfis.java
@@ -1,6 +1,6 @@
 package tcc.ifes.algoritmos;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,31 +11,46 @@ import tcc.ifes.model.ItemTag;
 import tcc.ifes.model.Usuario;
 import tcc.ifes.repositories.AvaliacaoRepository;
 import tcc.ifes.repositories.ItemTagRepository;
+import tcc.ifes.repositories.TagRepository;
 import tcc.ifes.repositories.UsuarioRepository;
 
 @Configuration
 public class CriandoPerfis {
-	
+
 	@Autowired
 	UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	ItemTagRepository itemTagRepository;
-	
+
 	@Autowired
 	AvaliacaoRepository avaliacaoRepository;
-	
+
+	@Autowired
+	TagRepository tagRepository;
+	private List<ItemTag> tags = new ArrayList<ItemTag>();
+	private List<Avaliacao> avaliacao = new ArrayList<Avaliacao>();
+
 	public Usuario showUsuario() {
-		
-		Usuario usuario =  usuarioRepository.findOne(7);
-		List<Avaliacao> avaliacao = avaliacaoRepository.findByUsuario(usuario);
-		//List<Avaliacao> itens = avaliacaoRepository.findByItem(avaliacao);
+
+		Usuario usuario = usuarioRepository.findOne(1);
+		avaliacao = avaliacaoRepository.findByUsuario(usuario);
 		System.out.println(usuario + " " + avaliacao);
-		for(int i = 0; i < avaliacao.size(); i++) {
-				System.out.println(avaliacao.get(i).getItem().getItens());
+		for (int i = 0; i < avaliacao.size(); i++) {
+			for(ItemTag item : avaliacao.get(i).getItem().getItens()) {
+				item.setTag(tagRepository.findOne(item.getTag().getId()));
+				item.setItem(avaliacao.get(i).getItem());
+				System.out.println(item);
+				tags.add(item);
 			}
+		}
+		System.out.println(tags);
+
+		return usuario;
+	}
 	
-	return usuario;
+	
+	public void criandoMatriz() {
 	}
 
 }
